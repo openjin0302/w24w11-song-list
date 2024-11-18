57c3c51cf4113af8f56c603c2d4633cafe244c40
openjin0302 <alswoquf456@gmail.com>
2024-11-18 18:46:39 +0900

    💄 노래 출력

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ function App() {
   return (
     <div>
       <Header/>
+      <Playlist/>
+      <Container/>
     </div>
   )
 }
@@ -11,6 +13,25 @@ function App() {
 function Header(){
   return (
     <h1>React 프로그래밍</h1>
+  )
+}
+
+function Playlist(){
+  return (
+    <div className="playlist">
+      프로그래밍하면서 듣고 싶은 노래
+    </div>
+  )
+}
+
+function Container(){
+  return(
+    <div className = "container">
+      <a href="https://www.youtube.com/results?search_query=슈퍼노바">
+      <img src="https://picsum.photos/600/150?random=1" alt="랜덤 이미지 1"/>
+      <div className="song-title">슈퍼노바</div>
+      </a>
+    </div>
   )
 }
 
