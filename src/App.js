4dd95e9d9f62ded2d42c045089527a86037165f9
openjin0302 <alswoquf456@gmail.com>
2024-11-28 13:51:13 +0900

    💄 노래 정보 배열 사용

--- src/App.js
+++ src/App.js
@@ -4,9 +4,44 @@ import Container from './Container';
 const App = () => {
 
   const songs = [
-    "리와인드",
-    "슈퍼노바",
-    "사건의 지평선",
+    {
+      id: 1,
+      title: "APT",
+      singer: "로제 & 브루노 마스",
+      rating: 4,
+      lyrics: `채영이가 좋아하는
+랜덤 게임
+랜덤 게임
+Game start
+아파트, 아파트
+아파트, 아파트
+아파트, 아파트
+Uh, uh-huh, uh-huh
+아파트, 아파트
+아파트, 아파트
+아파트, 아파트
+Uh, uh-huh, uh-huh
+Kissy face, kissy face
+Sent to your phone, but
+I'm tryna kiss your lips for real (uh-huh, uh-huh)
+Red hearts, red hearts
+That's what I'm on, yeah
+Come give me somethin' I can feel, oh-oh-oh`,
+    },
+    {
+      id: 2,
+      title: "사건의 지평선",
+      singer: "윤하",
+      rating: 5,
+      lyrics: null,
+    },
+    {
+      id: 3,
+      title: "POWER",
+      singer: "GDRAGON",
+      rating: 4,
+      lyrics: null,
+    },
   ]
 
 
@@ -31,8 +66,8 @@ const Playlist = props => {
     <div className='playlist'>
       <div className="playlist">{props.title}</div>
       {
-        props.listsong.map((song,index)=>(
-         <Container key={index} title={song} index={index}/>
+        props.listsong.map(song=>(
+          <Container key={song.id} song={song}/>
         ))
       }
     </div>
--- src/Container.js
+++ src/Container.js
@@ -1,11 +1,24 @@
 const Container = (props)  =>{
     return(
-      <div className = "container">
-        <a href={`https://www.youtube.com/results?search_query=${props.title}`}>
-        <img src={`https://picsum.photos/600/150?random=${props.index}}`} alt={`랜덤 이미지 ${props.index}}`} />
-        <div className="song-title">{props.title}</div>
-        </a>
-      </div>
+      <>
+        <div className="container">
+          <a href={`https://www.youtube.com/results?search_query=${props.song.title}`}
+          target="_blank"
+          rel="noreferrer">
+            <img 
+              src={`https://picsum.photos/600/150?random=${props.song.id}`} 
+              alt={`랜덤 이미지 ${props.song.id}`}/>
+            <div className="song-title">
+              {`${props.song.title} (${props.song.singer})`}
+            </div>
+          </a>
+        </div>
+        {props.song.lyrics && (
+          <pre className="song-lyrics">
+            {props.song.lyrics}
+          </pre>
+        )}
+      </>
     )
   }
 
